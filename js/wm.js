68c0274dd0a9ee1ed141efeb2b2c12e84025e7c7
Sergey Kizenko <sergey.kizenko@walkme.com>
2020-07-30 16:38:51 +0300

    etst

---
+++ js/wm.js
@@ -0,0 +1,2 @@
+(function() {var walkme = document.createElement('script'); walkme.type = 'text/javascript'; walkme.async = true; walkme.src = 'https://cdn.walkmeqa.com/users/7239b814bcf245e2a9dbe3d577fbd147/test/walkme_7239b814bcf245e2a9dbe3d577fbd147_https.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(walkme, s); window._walkmeConfig = {smartLoad:true}; })();
+
